ced4199c87bb79ea9aa21401bb7b3b7c4e9caa15
ploy <ployw@unr.edu>
2024-08-08 16:37:36 -0700

    adjust svg, add buttonActive

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import "./ProjectCardStyles.css"
 
 const ProjectCard = ({
@@ -10,13 +10,21 @@ const ProjectCard = ({
   link,
   buttonText
 }) => {
+  const [buttonActive, setButtonActive] = useState(false);
+  
+  useEffect(() => {
+    if(link) {
+      setButtonActive(true);
+    }
+  }, [link]);
+
   return (
     <div className="card container">
       {imgSrc && imgAlt && (<img className="card-img" src={imgSrc} alt={imgAlt}>
       </img>)}
       {title && (<h1 className="card-title">{title}</h1>)}
       {description && (<p className="card-description">{description}</p>)}
-      {items && (<div className="card-items container inner">
+      {items && (<div className={buttonActive ? "card-items container inner button-active" : "card-items container inner button-inactive"}>
         <ul className="card-items-list">
           {items.map((item, index) => (
             <li className="card-item card-highlight" key={index}>{item}</li>
