00ef8fddf08930329b166f29ec61d5154de2e590
ploy <ployw@unr.edu>
2024-10-12 21:22:12 -0700

    hero height on mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
     <div className="home container inner inner-div">
       <div className="about-content inner">
         <div className="about-me inner">
-          <h2>Hello, I'm <span className="highlight">Ploy Wandeevong</span>!</h2>
-          <p>I am currently a junior studying Computer Science and Engineering with an interest in full-stack development. I enjoy learning new skills and applying them to build fun, practical, and challenging projects. I'm always looking to learn, collaborate with others, and grow as a developer!</p>
+          <h2>Hello, I'm <span className="highlight">Ploy</span>!</h2>
+          <p>I am currently an undergrad junior interested in full-stack development. I enjoy learning new skills and applying them to build fun, practical, and challenging projects. I'm always looking to learn, collaborate with others, and grow as a developer!</p>
         </div>
         <div className="about-links container inner">
             <a target="_blank" className="link" href="https://github.com/ployw" rel="noopener noreferrer">
@@ -21,6 +21,7 @@ const Hero = () => {
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" aria-hidden="true" focusable="false" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"></path></svg>
               </button>
             </a>
+            <p className="highlight">ployprim9@gmail.com</p>
         </div>
       </div>
     </div>
